cad53d3b37b05307a33ec6e45491c6503a9e19bd
Inna Ratushna <Ratushnaya.uv@gmail.com>
2024-07-03 14:34:07 +0300

    change some code

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,24 @@
 
 'use strict';
 
-const categoriesItem = document.querySelectorAll('.item');
-console.log(`Number of categories: ${categoriesItem.length}`);
+const categoriesItem = document.querySelectorAll('.item'); // отримуємо доступ до улменту через клас;
+console.log(`Number of categories: ${categoriesItem.length}`); // виводимо в консоль кількість елементів li.item;
 
-categoriesItem.forEach(category => {
+categoriesItem.forEach(category => { // функція, що виводить в консоль текст заголовка та кількість елемнтів у категорії(усіх li, вкладених у нього)
     console.log(`Category: ${category.firstElementChild.textContent}`);
     console.log(`Elemets: ${category.lastElementChild.children.length}`);
 })
 
+
+
+
+/*З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
+
+Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
+
+
+На що буде звертати увагу ментор при перевірці:
+
+Кількість категорій, їх назва та кількість елементів отримані за допомогою властивостей і методів DOM-елементів
+Дані за кожною категорією були отримані й виведені в консоль у тілі циклу або методу forEach() */
--- js/task-2.js
+++ js/task-2.js
@@ -28,7 +28,7 @@ const images = [
 ];
 
 
-const listImags = document.querySelector('.gallery');
+const listImags = document.querySelector('.gallery'); 
 
 const makeImagesItems = ({ url, alt }) => `<li><img src="${url}" width="400" alt="${alt}"></img></li>`;
 
@@ -42,3 +42,25 @@ listImags.style.display = 'flex';
 listImags.style.flexWrap = 'column-reverse';
 listImags.style.gap = '30px';
 listImags.style.jystifyContent = 'center';
+
+
+/*Напиши скрипт для створення галереї зображень на основі масиву даних. HTML містить список ul.gallery.
+
+<ul class="gallery"></ul>
+
+
+
+Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>.
+
+Ти можеш створити й додати HTML-елементи, використовуючи document.createElement() і elem.append() або шаблонні рядки і elem.insertAdjacentHTML().
+
+Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+Додай мінімальне оформлення галереї флексбоксами через CSS класи.
+
+На що буде звертати увагу ментор при перевірці:
+
+Створена й додана в DOM галерея із трьох зображень
+Галерея додана у список ul.gallery і являє собою 3 елементи <li>, в які вкладені елементи <img>
+Для створення елементів <img> використані дані з масиву об’єктів images
+Усі елементи галереї додані в DOM за одну операцію додавання
+Є мінімальне оформлення галереї флексбоксами через CSS класи*/
